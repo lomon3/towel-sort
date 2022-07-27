9be60578024301f49e99c6cf864ba50d580aab9b
lomon3 <lomon3@gmail.com>
2022-07-27 21:24:08 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-function towelSort(arr) {
+/* function towelSort(arr) {
     let res = [];
     arr.forEach(function(element, index) {
         (index % 2 == 0) ? res.push(...element): res.push(...element.reverse());
     });
     return res;
-}
+} */
 // You should implement your task here.
 
+console.log(towelSort([]));
 module.exports = function towelSort(matrix) {
-    return [];
+    let res = [];
+    matrix.forEach(function(element, index) {
+        (index % 2 == 0) ? res.push(...element): res.push(...element.reverse());
+    });
+    return res;
 }
